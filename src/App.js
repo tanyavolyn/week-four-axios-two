1d1e1ba218b8cbcddea71c6beb7091f176c32ed5
tanyavolyn <tanyavolyn@gmail.com>
2022-12-20 19:49:32 +0100

    your massage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Component } from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends Component{
+  state={
+    posts: []
+  }
+componentDidMount(){
+  axios.get("https://jsonplaceholder.typicode.com/posts")
+  .then(res=>{
+    const posts = res.data;
+    this.setState({posts})
+  })
+}
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      {this.state.posts.map(posts=>
+
+      <div>
+      <p key={posts.id}>{posts.title}</p>
+      <p key={posts.id}>{posts.body}</p>
+      </div>
+      )
+  }
     </div>
   );
-}
+}}
 
 export default App;
